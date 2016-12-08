5b7a57a085ea89063c3f2ccdbe6da286c0a1a6c4
Lionhear21 <chicodj014@hotmail.cl>
2016-12-07 22:06:32 -0300

    pene

--- src/proyecto3poo/MainCanvas.java
+++ src/proyecto3poo/MainCanvas.java
@@ -6,12 +6,25 @@
 package proyecto3poo;
 
 import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
+import javafx.event.Event;
 import javafx.event.EventHandler;
+import javafx.scene.canvas.Canvas;
 
 /**
  *
  * @author chico
  */
-public class MainCanvas extends  implements EventHandler, ChangeListener {
+public class MainCanvas extends Canvas implements EventHandler, ChangeListener {
+
+    @Override
+    public void handle(Event t) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void changed(ObservableValue ov, Object t, Object t1) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
     
 }
