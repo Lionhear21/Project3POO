1460a2134f6e5fc2739a4325a96255cac713b286
William Benítez Núñez <willyjoearmstrong@gmail.com>
2016-12-14 12:09:28 -0300

    tipo

--- src/project3poo/Tipo.java
+++ src/project3poo/Tipo.java
@@ -9,19 +9,26 @@ package project3poo;
  *
  * @author Elio
  */
-public enum Tipo {
-    
+public enum Tipo 
+{
     AZUL("Blue_Candy.png"),
     VERDE("Green_Candy.png"),
     NARANJO("Orange_Candy.png"),
     PURPURA("Purple_Candy.png"),
     ROJO("Red_Candy.png"),
     AMARILLO("Yellow_Candy.png"),
-    NULL("null.png");
+    NULL("null.png"),
+    AZUL_SELECTED("Blue_Candy_selected.png"),
+    VERDE_SELECTED("Green_Candy_selected.png"),
+    NARANJO_SELECTED("Orange_Candy_selected.png"),
+    PURPURA_SELECTED("Purple_Candy_selected.png"),
+    ROJO_SELECTED("Red_Candy_selected.png"),
+    AMARILLO_SELECTED("Yellow_Candy_selected.png");
     
     private final String filename;
+    private final String filenameSelected;
     
-    private Tipo(String filename)
+    private Tipo(String filename, String filenameSelected)
     {
         this.filename = filename;
     }
